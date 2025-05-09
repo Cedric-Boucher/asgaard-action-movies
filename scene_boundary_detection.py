eba53f1ce9878b567610268fcb54ed46fa9286b7
Cedric Boucher <cjbouche@ualberta.ca>
2025-05-09 10:13:05 -0600

    create functions to get window similarities for all windows

--- scene_boundary_detection.py
+++ scene_boundary_detection.py
@@ -1,8 +1,14 @@
 from PIL import Image
 import numpy as np
 from scipy.stats import skew, kurtosis
-from collections.abc import Callable, Iterable
+from collections.abc import Callable, Iterable, Generator
 import math
+import argparse
+import cv2
+import os
+from tqdm import tqdm
+import csv
+from itertools import islice
 
 # TODO: need some ground truth to be able to do:
 # TODO: need to train something to determine good feature weights, could do GA
@@ -172,3 +178,84 @@ def window_similarity(left_window_shots: list[Iterable[Image.Image]], right_wind
     )
 
     return window_similarity
+
+def video_window_similarities(video_path: str, shots_csv_path: str, left_window_size: int, right_window_size: int, feature_weights: tuple[float, ...]) -> Generator[float, None, None]:
+    total_shot_count: int = video_total_shot_count(video_path)
+    sliding_window_count: int = total_shot_count - left_window_size - right_window_size + 1
+    frames: Iterable[Image.Image] = video_to_frames(video_path)
+    shot_frame_ranges: Iterable[tuple[int, int]] = video_shot_frame_ranges(shots_csv_path)
+    shot_frames: Iterable[Iterable[Image.Image]] = video_shot_frames(frames, shot_frame_ranges)
+
+    left_window_shots: list[Iterable[Image.Image]] = list(islice(shot_frames, left_window_size))
+    right_window_shots: list[Iterable[Image.Image]] = list(islice(shot_frames, right_window_size))
+    for _ in tqdm(range(sliding_window_count), desc="Computing Window Similarities", total=sliding_window_count):
+        left_window_shots.pop(0)
+        left_window_shots.append(right_window_shots.pop(0))
+        right_window_shots.append(next(shot_frames))
+
+        yield window_similarity(left_window_shots, right_window_shots, feature_weights)
+
+def video_to_frames(video_path: str) -> Generator[Image.Image, None, None]:
+    video = cv2.VideoCapture(video_path)
+    total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    for _ in range(total_frames):
+        ret, frame = video.read()
+        if not ret:
+            break
+
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame_image: Image.Image = Image.fromarray(frame_rgb)
+        
+        yield frame_image  # Yield one frame at a time
+
+    video.release()
+
+def video_total_frame_count(video_path: str) -> int:
+    video = cv2.VideoCapture(video_path)
+    total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    return total_frames
+
+def video_shot_frame_ranges(shots_csv_path: str) -> Generator[tuple[int, int], None, None]:
+    with open(shots_csv_path, "r") as shots_csv:
+        shots_csv_reader = csv.reader(shots_csv)
+        for row in shots_csv_reader:
+            if row[0].isnumeric() and row[1].isnumeric() and len(row) == 4:
+                # skip header / last / invalid rows
+                yield (int(row[0]), int(row[1]))
+
+def video_total_shot_count(shots_csv_path: str) -> int:
+    shot_count: int = 0
+    with open(shots_csv_path, "r") as shots_csv:
+        shots_csv_reader = csv.reader(shots_csv)
+        for row in shots_csv_reader:
+            shot_count += int(row[0].isnumeric() and row[1].isnumeric() and len(row) == 4)
+
+    return shot_count
+
+def video_shot_frames(frames: Iterable[Image.Image], shot_frame_ranges: Iterable[tuple[int, int]]) -> Generator[Iterable[Image.Image], None, None]:
+    last_shot_frame_end: int = -1
+    for shot_frame_start, shot_frame_end in shot_frame_ranges:
+        assert shot_frame_start == last_shot_frame_end + 1
+        shot_frame_count: int = shot_frame_end - shot_frame_start + 1
+        shot_frames: Iterable[Image.Image] = islice(frames, shot_frame_count)
+
+        yield shot_frames
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Shot Generator Using Cosine Similarity")
+    parser.add_argument('--video', required=True, type=str, help="Path to Video File")
+    parser.add_argument('--shots', required=True, type=str, help="Path to CSV Containing Shots")
+    parser.add_argument('--left-window-size', type=int, default=3, help="Number of Shots in Left Sliding Window")
+    parser.add_argument('--right-window-size', type=int, default=3, help="Number of Shots in Right Sliding Window")
+    args = parser.parse_args()
+    video_path: str = args.video
+    assert os.path.exists(video_path) and os.path.isfile(video_path), "Could not find a file at the provided video path"
+    shots_csv_path: str = args.shots
+    assert os.path.exists(shots_csv_path) and os.path.isfile(shots_csv_path), "Could not find a file at the provided shots CSV path"
+    left_window_size: int = args.left_window_size
+    assert left_window_size > 0, "left window size must be greater than 0"
+    right_window_size: int = args.right_window_size
+    assert right_window_size > 0, "right window size must be greater than 0"
